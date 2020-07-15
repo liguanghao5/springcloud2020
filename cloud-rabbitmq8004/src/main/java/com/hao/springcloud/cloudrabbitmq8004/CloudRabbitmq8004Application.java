e984535f07a4332f862c7b8bbf502a8656673289
liguanghao <liguanghao@91taogu.com>
2020-07-15 15:26:48 +0800

    引入rabbitmq

---
+++ cloud-rabbitmq8004/src/main/java/com/hao/springcloud/cloudrabbitmq8004/CloudRabbitmq8004Application.java
@@ -0,0 +1,14 @@
+package com.hao.springcloud.cloudrabbitmq8004;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudRabbitmq8004Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudRabbitmq8004Application.class,args);
+    }
+
+}
