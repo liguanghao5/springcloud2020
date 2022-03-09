d12f22e8b3ec1f1829915f5efb79d8a42f564292
liguanghao <lgh123456>
2022-03-09 15:32:57 +0800

    openfeign加sentinel

--- cloud-openfeign9001/src/main/java/com/hao/openfeign9001/service/User1Service.java
+++ cloud-openfeign9001/src/main/java/com/hao/openfeign9001/service/User1Service.java
@@ -3,7 +3,7 @@ package com.hao.openfeign9001.service;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(value = "openfeign9001")
+@FeignClient(value = "openfeign9002")
 public interface User1Service {
 
     @GetMapping("/getUserName")
--- cloud-openfeign9002/src/main/java/com/hao/cloudopenfeign9002/service/UserService.java
+++ cloud-openfeign9002/src/main/java/com/hao/cloudopenfeign9002/service/UserService.java
@@ -1,6 +1,8 @@
 package com.hao.cloudopenfeign9002.service;
 
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +13,15 @@ public class UserService {
 
 
     @GetMapping("/getUserName")
+    @SentinelResource(value = "getUserName",blockHandler = "blo_getUserName")
     public String getUserName() {
 
         log.info("user1微服务的userName");
         return "user1微服务的userName";
+    }
+
+    public String blo_getUserName(BlockException exception){
+        return "活动太火爆了，请稍后重试";
     }
 
 
